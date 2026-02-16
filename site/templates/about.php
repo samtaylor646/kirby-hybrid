25c0938fcab9f404c671cb8e6da44b8a710bfe11
Sam Taylor <samtaylorcreative@gmail.com>
2026-02-16 12:13:24 -0500

    about php yml
    update about files

--- site/templates/about.php
+++ site/templates/about.php
@@ -1,51 +1,36 @@
-<?php
-/*
-  Templates render the content of your pages.
-
-  They contain the markup together with some control structures
-  like loops or if-statements. The `$page` variable always
-  refers to the currently active page.
-
-  To fetch the content from each field we call the field name as a
-  method on the `$page` object, e.g. `$page->title()`.
-
-  This about page example uses the content from our layout field
-  to create most parts of the page and keeps it modular. Only the
-  contact box at the bottom is pre-defined with a set of fields
-  in the about.yml blueprint.
-
-  Snippets like the header and footer contain markup used in
-  multiple templates. They also help to keep templates clean.
-
-  More about templates: https://getkirby.com/docs/guide/templates/basics
-*/
-?>
 <?php snippet('header') ?>
-<?php snippet('intro') ?>
-<?= $page->mainContent()->toBlocks() ?>
-<?php snippet('layouts', ['field' => $page->layout()])  ?>
+
+<main>
+  <?php // 1. Render the Hero blocks (mainContent) first ?>
+  <?= $page->mainContent()->toBlocks() ?>
+
+  <?php // 2. Render the modular layouts ?>
+  <?php snippet('layouts', ['field' => $page->layout()]) ?>
+</main>
 
 <aside class="contact">
-  <h2 class="h1">Get in contact</h2>
-  <div class="grid" style="--gutter: 1.5rem">
-    <section class="column text" style="--columns: 4">
-      <h3>Address</h3>
-      <?= $page->address() ?>
-    </section>
-    <section class="column text" style="--columns: 4">
-      <h3>Email</h3>
-      <p><?= Html::email($page->email()) ?></p>
-      <h3>Phone</h3>
-      <p><?= Html::tel($page->phone()) ?></p>
-    </section>
-    <section class="column text" style="--columns: 4">
-      <h3>On the web</h3>
-      <ul>
-        <?php foreach ($page->social()->toStructure() as $social): ?>
-        <li><?= Html::a($social->url(), $social->platform()) ?></li>
-        <?php endforeach ?>
-      </ul>
-    </section>
+  <div class="container">
+    <h2 class="h1">Get in contact</h2>
+    <div class="grid" style="--gutter: 1.5rem">
+      <section class="column text" style="--columns: 4">
+        <h3>Address</h3>
+        <?= $page->address()->kt() ?>
+      </section>
+      <section class="column text" style="--columns: 4">
+        <h3>Email</h3>
+        <p><?= Html::email($page->email()) ?></p>
+        <h3>Phone</h3>
+        <p><?= Html::tel($page->phone()) ?></p>
+      </section>
+      <section class="column text" style="--columns: 4">
+        <h3>On the web</h3>
+        <ul>
+          <?php foreach ($page->social()->toStructure() as $social): ?>
+          <li><?= Html::a($social->url(), $social->platform()) ?></li>
+          <?php endforeach ?>
+        </ul>
+      </section>
+    </div>
   </div>
 </aside>
 
